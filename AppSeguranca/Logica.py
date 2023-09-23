4af3d5512798e16e218c4173b0d6d135e67d75ca
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-09-23 00:00:15 -0300

    Busca por CEP

--- AppSeguranca/Logica.py
+++ AppSeguranca/Logica.py
@@ -1,9 +1,25 @@
-# class Cadastro:
-#     def cadastro(self):
-#         layout=\
-#         [
-#             sg.Push(), sg.Text('...'),sg.Push()
-#         ]
-#         return sg.Window('Entrar',layout)
-#
-# Cadastro.cadastro().read()
+#-------------------------------------------
+# CEP
+#-------------------------------------------
+import json
+import requests
+
+cep = input('Digite o CEP: ')
+def obterCEP():
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    resposta = requests.get(url)
+    data = json.loads(resposta.content)
+    return data
+    # if resposta.status_code == 200:
+    #    data = json.loads(resposta.content)
+    #    print('CEP: ', data['cep'])
+    #    print('Logradouro: ', data['logradouro'])
+    #    print('Complemento: ', data['complemento'])
+    #    print('Bairro: ', data['bairro'])
+    #    print('Cidade: ', data['localidade'])
+    #    print('Estado: ', data['uf'])
+    # else:
+    #     print('Não foi possível obter informações do CEP informado.')
+    # return
+# -------------------------------------------
+print(obterCEP())
