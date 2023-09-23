39edc6bf791eeb384c6938eaaad8512e63a5387d
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-09-23 09:22:14 -0300

    Busca por CEP

--- AppSeguranca/Logica.py
+++ AppSeguranca/Logica.py
@@ -1,25 +1,16 @@
-#-------------------------------------------
+# -------------------------------------------
 # CEP
-#-------------------------------------------
+# -------------------------------------------
 import json
 import requests
 
-cep = input('Digite o CEP: ')
-def obterCEP():
-    url = f"https://viacep.com.br/ws/{cep}/json/"
-    resposta = requests.get(url)
-    data = json.loads(resposta.content)
-    return data
-    # if resposta.status_code == 200:
-    #    data = json.loads(resposta.content)
-    #    print('CEP: ', data['cep'])
-    #    print('Logradouro: ', data['logradouro'])
-    #    print('Complemento: ', data['complemento'])
-    #    print('Bairro: ', data['bairro'])
-    #    print('Cidade: ', data['localidade'])
-    #    print('Estado: ', data['uf'])
-    # else:
-    #     print('Não foi possível obter informações do CEP informado.')
-    # return
+
+def obterCEP(cep):
+    url = 'https://viacep.com.br/ws/{}/json/'.format(cep)
+    requisicao = requests.get(url)
+    endereco = json.loads(requisicao.content)
+    return endereco
 # -------------------------------------------
-print(obterCEP())
+
+cep = input('Digite o CEP: ')
+print(obterCEP(cep))
