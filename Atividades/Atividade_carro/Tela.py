4fbd0ecd70e5a3d9a65caf9a16c4d419f0de4195
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-10-18 18:49:25 -0300

    Atividade 19.10.23

--- Atividades/Atividade_carro/Tela.py
+++ Atividades/Atividade_carro/Tela.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from PySimpleGUI import (
     Input, Window, Button,
     Frame, Text, theme,
@@ -7,23 +8,27 @@ from PySimpleGUI import (
 
 
 def marcha_carro():
-    theme('Blue')
+    theme('BluePurple')
 
     layout = [
         [
-            Text(text='Velocidade atual')
+            Text(text='Velocidade em km/h')
         ],
 
         [
-            Input(key='velocidade'),
+            Input(key='-VEL-',
+                  size=(25, 1),
+                  justification='center'),
 
-            Button(target='velocidade')
+            Button(button_text='Testar',
+                   target='-VEL-',
+                   key='-BVEL-')
         ]
     ]
 
     layout = [
         [
-            Frame(title='Teste',
+            Frame(title='Simulação',
                   layout=layout)
         ]
     ]
@@ -36,7 +41,24 @@ janela = marcha_carro()
 
 while True:
     evento, valor = janela.read()
-    if WIN_CLOSED:
+    if evento == WIN_CLOSED:
         break
-
+    if '-BVEL-' in evento:
+        num = valor['-VEL-']
+        num = int(num)
+        if num == 0:
+            popup('Neutro')
+        if num <= 20:
+            popup('1° Marcha')
+        if 20 > num >= 40:
+            popup('2° Marcha')
+        if 40 > num >= 50:
+            popup('3° Marcha')
+        if 50 > num >= 60:
+            popup('4° Marcha')
+        if num > 60:
+            popup('5° Marcha')
+        else:
+            popup('Valor inválido')
+    janela['-VEL-'].update('')
 janela.close()
